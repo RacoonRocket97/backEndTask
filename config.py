432cbd26cae454dbbaf97151ccfe7413b617659b
RacoonRocket97 <bekzat.kumarbek@narxoz.kz>
2025-05-07 23:15:53 +0500

    add wsgi

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 from datetime import timedelta
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-very-secret-key-123'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from run import app  # If your main file is app.py
+
+if __name__ == "__main__":
+    app.run()
